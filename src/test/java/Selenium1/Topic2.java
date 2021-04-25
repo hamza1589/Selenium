1a0869ad9588a0469e16c9f2309aa33ff5444aef
hamza <hamzacherfaoui32@yahoo.com>
2021-04-25 03:57:32 -0400

    Update selenium

--- src/test/java/Selenium1/Topic2.java
+++ src/test/java/Selenium1/Topic2.java
@@ -2,8 +2,11 @@ package Selenium1;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+
+import javax.swing.*;
 
 public class Topic2 extends TestBase {
     @Test
@@ -36,6 +39,20 @@ public class Topic2 extends TestBase {
         WebElement element=driver.findElement(By.cssSelector("#gh-cat"));
         Select slct=new Select(element);
         slct.selectByValue("11233");
+        sleepFor(2);
+        closeDriver();
+
+    }
+    @Test
+    public void mouseHover(){
+        setUpDriver("chrome");
+        setupUrl("https://www.ebay.com");
+        sleepFor(2);
+        WebElement motor=driver.findElement(By.linkText("Motors"));
+        Actions act =new Actions(driver);
+        act.moveToElement(motor).build().perform();
+        sleepFor(2);
+        driver.findElement(By.linkText("Cars & Trucks")).click();
 
     }
 }
